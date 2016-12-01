a793538d99f3b1108b4029dfb1fa9762c79437d7
Yuji <kevinhuangdreamer@gmail.com>
2016-12-01 18:44:11 +0800

    exec shrink on window loaded

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
+function shrink() {
+  var paragraphs = document.querySelectorAll("._58jw");
+  for(var i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.fontSize = "15px";
+    paragraphs[i].style.lineHeight = 1.38;
+  }
+}
 window.onload = function() {
-  setInterval(function() {
-    var paragraphs = document.querySelectorAll("._58jw");
-    for(var i = 0; i < paragraphs.length; i++) {
-      paragraphs[i].style.fontSize = "15px";
-      paragraphs[i].style.lineHeight = 1.38;
-    }
-  }, 1000);
+  shrink();
+  setInterval(shrink, 1000);
 }
