33ec67766afaa5bc263c8eb5a6f8d5d2f277cf20
Yuji <kevinhuangdreamer@gmail.com>
2016-12-01 21:06:08 +0800

    add font-weight

--- content.js
+++ content.js
@@ -3,9 +3,10 @@ function shrink() {
   for(var i = 0; i < paragraphs.length; i++) {
     paragraphs[i].style.fontSize = "15px";
     paragraphs[i].style.lineHeight = 1.38;
+    paragraphs[i].style.fontWeight = "normal";
   }
 }
-window.onload = function() {
+// window.onload = function() {
   shrink();
   setInterval(shrink, 1000);
-}
+// }
